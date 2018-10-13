cbf31bf6a05fbae8b33b100f4698beee880ca6be
jhalmar1888 <jhalmar1888@gmail.com>
2018-10-12 20:35:48 -0400

    imagenes dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,10 +24,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        echo 'Formato #1 ' . NumerosEnLetras::convertir(1988208.99) . '<br>';
-        
-        echo 'Formato #2 ' . NumerosEnLetras::convertir(1988208.99,'Bolivianos',false,'Centavos') . '<br>';
-        
-        echo 'Formato #3 ' . NumerosEnLetras::convertir(1988208.99,'Bolivianos',true) . '<br>';
+        return view('index');
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,80 @@
 @extends('layouts.app')
 @section('content')
+<div id="home" class="content has-bg home" style="height: 969px;">
+            <!-- begin content-bg -->
+            <div class="content-bg">
+            </div>
+           <h1 class="text-center">ORDENES DE PAGO </h1>
+<h1 class="page-header">Dashboard <small>del sistema de Orden de Pago</small></h1>
+		<div class="container home-content">
+				<!-- begin col-3 -->
+				<div class="col-md-3 col-sm-6">
+					<div class="widget widget-stats bg-green">
+						<div class="stats-icon"><i class="fa fa-desktop"></i></div>
+						<div class="stats-info">
+							<h4>TOTAL VISITORS</h4>
+							<p>3,291,922</p>	
+						</div>
+						<div class="stats-link">
+							<a href="javascript:;">View Detail <i class="fa fa-arrow-circle-o-right"></i></a>
+						</div>
+					</div>
+				</div>
+				<!-- end col-3 -->
+				<!-- begin col-3 -->
+				<div class="col-md-3 col-sm-6">
+					<div class="widget widget-stats bg-blue">
+						<div class="stats-icon"><i class="fa fa-chain-broken"></i></div>
+						<div class="stats-info">
+							<h4>BOUNCE RATE</h4>
+							<p>20.44%</p>	
+						</div>
+						<div class="stats-link">
+							<a href="javascript:;">View Detail <i class="fa fa-arrow-circle-o-right"></i></a>
+						</div>
+					</div>
+				</div>
+				<!-- end col-3 -->
+				<!-- begin col-3 -->
+				<div class="col-md-3 col-sm-6">
+					<div class="widget widget-stats bg-purple">
+						<div class="stats-icon"><i class="fa fa-users"></i></div>
+						<div class="stats-info">
+							<h4>UNIQUE VISITORS</h4>
+							<p>1,291,922</p>	
+						</div>
+						<div class="stats-link">
+							<a href="javascript:;">View Detail <i class="fa fa-arrow-circle-o-right"></i></a>
+						</div>
+					</div>
+				</div>
+				<!-- end col-3 -->
+				<!-- begin col-3 -->
+				<div class="col-md-3 col-sm-6">
+					<div class="widget widget-stats bg-red">
+						<div class="stats-icon"><i class="fa fa-clock-o"></i></div>
+						<div class="stats-info">
+							<h4>AVG TIME ON SITE</h4>
+							<p>00:12:23</p>	
+						</div>
+						<div class="stats-link">
+							<a href="javascript:;">View Detail <i class="fa fa-arrow-circle-o-right"></i></a>
+						</div>
+					</div>
+				</div>
+				<!-- end col-3 -->
+			</div>
 
-desde aqui
+<div class="bg-cover">
+    <img src="/images/cofadena.png" alt="">
+
+    <img src="/images/cofadena2.png" width="500" height="500">
+</div>
+        </div>
+
+
+
+	
+
 
 @endsection
